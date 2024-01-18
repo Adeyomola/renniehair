d2ea45777595713eb494469460d268b82918cff7
Adeyomola <kayzeel@yahoo.com>
2024-01-18 18:02:47 +0100

    updated contact.js

--- js/contact.js
+++ js/contact.js
@@ -5,7 +5,14 @@ const Name = document.getElementById("name");
 const phone = document.getElementById("phone");
 const message = document.getElementById("textarea");
 
-singlebutton.addEventListener("click", () => {
-  const body = message.value + "%0D%0A" + Name.value + "%0D%0A" + phone.value;
-  window.location.href = `mailto:renniehair@gmail.com?subject=${Subject.value}&body=${body}`;
-});
+if (window.innerWidth < 1025) {
+  singlebutton.addEventListener("touchstart", () => {
+    const body = message.value + "%0D%0A" + Name.value + "%0D%0A" + phone.value;
+    window.location.href = `mailto:renniehair@gmail.com?subject=${Subject.value}&body=${body}`;
+  });
+} else {
+  singlebutton.addEventListener("click", () => {
+    const body = message.value + "%0D%0A" + Name.value + "%0D%0A" + phone.value;
+    window.location.href = `mailto:renniehair@gmail.com?subject=${Subject.value}&body=${body}`;
+  });
+}
